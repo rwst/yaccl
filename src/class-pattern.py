12b758e9059f4db1d0368ed230a33ab6d9cad86f
Ralf Stephan <ralf@ark.in-berlin.de>
2021-09-11 19:04:07 +0200

    split query

--- src/class-pattern.py
+++ src/class-pattern.py
@@ -18,36 +18,46 @@ script = os.path.basename(sys.argv[0])[:-3]
 OFFSET = 1000000000
 
 if dontquery is False:
-    print('performing query...', file=sys.stderr)
-    ret = os.popen('wd sparql class-pattern.rq >class-pattern.json'.format(script, script))
+    print('performing query 1...', file=sys.stderr)
+    ret = os.popen('wd sparql class-pattern1.rq >class-pattern1.json'.format(script, script))
     if ret.close() is not None:
         raise
-    ff = open('class-pattern.json'.format(script))
-    s = ff.read()
-    jol = json.loads(s)
-    with open('data-class-pattern.json', 'w+') as f:
-        f.write(json.dumps(sorted(jol, key=lambda data: int(data.get('item').get('value')[1:])), indent=0, ensure_ascii=False))
-else:
-    ff = open('data-class-pattern.json'.format(script))
-    s = ff.read()
-    jol = json.loads(s)
+ff = open('class-pattern1.json'.format(script))
+s = ff.read()
+jol = json.loads(s)
 
 items = set()
 iks = {}
 ik1 = {}
 smarts = {}
 smiles = {}
+outj = []
 for d in jol:
+    outj.append(d)
     it = d.get('item').get('value')
     items.add(it)
-    sma = d.get('p8533')
-    if sma is not None:
-        smarts[sma] = it
     ik = d.get('p235')
     if ik is not None:
         iks[ik] = it
         if ik[15:25] == 'UHFFFAOYSA':
             ik1[ik[:14]] = it
+
+if dontquery is False:
+    print('performing query 2...', file=sys.stderr)
+    ret = os.popen('wd sparql class-pattern2.rq >class-pattern2.json'.format(script, script))
+    if ret.close() is not None:
+        raise
+ff = open('class-pattern2.json'.format(script))
+s = ff.read()
+jol = json.loads(s)
+
+for d in jol:
+    outj.append(d)
+    it = d.get('item').get('value')
+    items.add(it)
+    sma = d.get('p8533')
+    if sma is not None:
+        smarts[sma] = it
     csm = d.get('p233')
     ism = d.get('p2017')
     smi = None
@@ -57,6 +67,9 @@ for d in jol:
         smi = csm
     if smi is not None:
         smiles[smi] = it
+
+with open('data-class-pattern.json', 'w+') as f:
+    f.write(json.dumps(sorted(outj, key=lambda data: int(data.get('item').get('value')[1:])), indent=0, ensure_ascii=False))
 
 print('#items: {}'.format(len(items)))
 print('#InChI keys: {}'.format(len(iks.keys())))
